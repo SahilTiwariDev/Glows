706a0b9445ea358cf51ecfdb543f44609165e115
SahilTiwariDev <sahiltiwari.contactme@gmail.com>
2025-05-02 16:29:58 +0530

    Backend completed

--- backend/models/userModels.js
+++ backend/models/userModels.js
@@ -1,31 +1,32 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
+const userSchema = mongoose.Schema(
+  {
     username: {
-        type: String,
-        required: true,
-        trim: true,
-    },  
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        trim: true,
-    },
-    password: {
-        type: String,
-        required: true,
-        trim: true,
-    },
-    isAdmin: {
-        type: Boolean,
-        required: true,
-        default: false,
+      type: String,
+      required: true,
     },
 
-},{
-    timestamps: true,
-});
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
+
 export default User;
